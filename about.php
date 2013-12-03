50d89dc2d350f0947e4825dc5d2899f69ce2ca2e
Branden Ogata <branden.s.ogata@gmail.com>
2013-12-03 10:02:59 -1000

    Issue #004: Adds further information to the About page.

--- about.php
+++ about.php
@@ -40,11 +40,27 @@
       <li>JavaScript
         <ul>
           <li>jQuery</li>
+          <li>AJAX</li>
         </ul>
       </li>
       <li>PHP</li>
       <li>Twitter Bootstrap</li>
+      <li>XAMPP
+        <ul>
+          <li>Apache</li>
+          <li>MySQL</li>
+        </ul>
+      </li>
+      <li>Server-Side Events</li>
     </ul>
+    
+    <p>
+      In particular, AJAX sends user invitations and moves to the server while server-side events 
+      allow the server to communicate invitation responses and opponent moves.  This project also
+      abuses the PHP keyword <b>include</b> to insert the same code in different locations.  For
+      example, the navbar is stored in its own PHP file, then <b>include</b>d in all of the pages
+      so that it appears at the top of every page.
+    </p>
   </div>
 </body>
 </html>
